a45f49dd57c1fcf7e2cccc1eeb178f1aedadd30a
Liam MACQUAIRE <liamdu92@gmail.com>
2025-05-02 15:06:52 +0200

    continue

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -3,11 +3,8 @@ import tseslint from "typescript-eslint";
 import vueParser from "vue-eslint-parser";
 import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
-import path from "path";
 
-
-
-const eslintConfigVue = [
+export const eslintConfigVue = [
 	...pluginVue.configs["flat/strongly-recommended"],
 	{
 		...duplojsEslintBase,
@@ -15,16 +12,60 @@ const eslintConfigVue = [
 			parser: vueParser,
 			parserOptions: {
 				parser: tseslint.parser,
-				project: path.resolve(__dirname, "./tsconfig.app.json"),
-				tsconfigRootDir: path.resolve(__dirname),
 				globals: globals.browser,
 				sourceType: "module",
+				projectService: true,
 				extraFileExtensions: [".vue"],
+				project: './tsconfig.json',
 			},
 		},
-		files: ["**/*.ts", "**/*.vue"],
-		ignores: ["**/*.test.ts", "eslint.config.ts"],
+		rules: {
+			...duplojsEslintBase.rules,
+			"vue/require-default-prop": "off",
+			"vue/html-indent": ["error", "tab"],
+			"no-undef": "off",
+			"max-len": "off",
+			"vue/max-len": [
+				"error",
+				{
+					code: 120,
+					template: 1000,
+				},
+			],
+			"vue/padding-line-between-tags": [
+				"error",
+				[
+					{
+						blankLine: "always",
+						prev: "*",
+						next: "*",
+					},
+				],
+			],
+			"vue/block-order": [
+				"error",
+				{
+					order: [["script", "template"], "style"],
+				},
+			],
+			"@stylistic/js/max-len": "off",
+			"@typescript-eslint/no-unsafe-argument": "off",
+			"@typescript-eslint/no-unsafe-call": "off",
+			"@typescript-eslint/no-unsafe-declaration-merging": "off",
+			"@typescript-eslint/no-unsafe-enum-comparison": "off",
+			"@typescript-eslint/no-unsafe-function-type": "off",
+			"@typescript-eslint/no-unsafe-member-access": "off",
+			"@typescript-eslint/no-unsafe-unary-minus": "off",
+			"@typescript-eslint/no-redundant-type-constituents": "off",
+			"id-length": [
+				"error",
+				{
+					min: 2,
+					exceptions: ["t"],
+				},
+			],
+		},
+		files: ["**/*.vue", "**/*.ts"],
+		ignores: ["**/*.test.ts"],
 	},
 ];
-
-export default eslintConfigVue;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,23 @@
-import path from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import {fileURLToPath} from "node:url";
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 	plugins: [
 		vue(),
 		tailwindcss(),
 		tsconfigPaths(),
 	],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
 	server: {
 		host: "0.0.0.0",
+		port: 3000,
 	},
 	optimizeDeps: {
 		force: true,
